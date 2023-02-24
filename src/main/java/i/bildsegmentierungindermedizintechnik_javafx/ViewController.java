4f10b2c23d712fd21acc6d26055de566fc430777
reney <rene.weissteiner@gmail.com>
2023-02-24 10:36:02 +0100

    o/

--- src/main/java/i/bildsegmentierungindermedizintechnik_javafx/ViewController.java
+++ src/main/java/i/bildsegmentierungindermedizintechnik_javafx/ViewController.java
@@ -14,26 +14,26 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class ViewController {
     View view;
-    ArrayList<Rectangle> recArray = new ArrayList<>();
-    ArrayList<Node> nodes;
+    ArrayList<Rectangle> recArray = new ArrayList<>(); // Array der zur Deko benötigten Dreiecke
+    ArrayList<Node> nodes; // Nodes mit Koordinaten zum kürzesten Weg
     Image polarCord;
 
     public ViewController(View view) {
         this.view = view;
         //Anzeigen des Polarbildes
 
-        //Provisorische Nodes zum Testen (Ersetzen mit der "build" methode, die ein Nodearray zurückgibt)
+        //Provisorische Nodes zum Testen (ersetzen mit der "build" Methode, die ein modeArray zurückgibt)
         nodes = new ArrayList<>();
         nodes.add(new Node(0,100, 1));
         nodes.add(new Node(100,300,1));
         nodes.add(new Node(200,50,1));
         nodes.add(new Node(300, 300, 1));
 
-        displayLayout();
+        displayLayout(); // Methode zum einfachen anzeigen der Buttons und Rechtecke
 
     }
 
-    private void dykHandler(ActionEvent event) {
+    private void dykHandler(ActionEvent event) { // sobald der dykButton ("Create Dijkstra") gedrückt wird, soll der kürzeste Weg eingezeichnet werden
         displayLine(nodes);
 
     }
@@ -41,17 +41,17 @@ public class ViewController {
         //Deko Rechtecke
         recArray.add(view.createRectangle(view.windowSizeX, view.windowSizeY/6, 0, 0, "#c2f2ee"));
         recArray.add(view.createRectangle(400,400,view.windowSizeX/2-200,view.windowSizeY/6+25,"#c2f2ee"));
-        view.view.getChildren().addAll(recArray);
+        view.view.getChildren().addAll(recArray); // zeigt das gesamte recArray an
 
-        //Buttons
+        //Button
         Button dykButton = new Button();
-        dykButton.setOnAction(this::dykHandler);
+        dykButton.setOnAction(this::dykHandler); // Wenn der Button gedrückt wird, soll er zu angegebener Methode springen.
         dykButton.setText("Create Dijkstra");
         dykButton.setPrefWidth(125);
         dykButton.setPrefHeight(recArray.get(0).getHeight()/3);
         dykButton.setLayoutY(recArray.get(0).getHeight()/3);
         dykButton.setLayoutX(view.windowSizeX-dykButton.getPrefWidth()-25);
-        view.view.getChildren().add(dykButton);
+        view.view.getChildren().add(dykButton); // Zeigt den Button an
 
     }
     public void displayLine(ArrayList<Node> nodeArray/*, String url*/){
@@ -62,18 +62,18 @@ public class ViewController {
         final int beginOfDisplayY = (int)(recArray.get(1).getY());
         System.out.println(beginOfDisplayY);
 
-            AtomicInteger i = new AtomicInteger(1);
-            KeyFrame keyFrame = new KeyFrame(new Duration(100), event -> {
+            AtomicInteger i = new AtomicInteger(1); // Counter zum Orientieren des nodeArrays
+            KeyFrame keyFrame = new KeyFrame(new Duration(100), event -> { // Die Linienführung erfolgt mittels einer kleinen Animation bzw. mit einer Verzögerung der einzelnen Linien
                 int startx = nodeArray.get(i.get() -1).x + beginOfDisplayX;
                 int starty = nodeArray.get(i.get() -1).y + beginOfDisplayY;
                 int endx = nodeArray.get(i.get()).x + beginOfDisplayX;
                 int endy = nodeArray.get(i.get()).y + beginOfDisplayY;
-                view.view.getChildren().add(view.createLine(startx, starty,  endx, endy));
+                view.view.getChildren().add(view.createLine(startx, starty,  endx, endy)); //Zeigt die Linie final auf dem Fenster an
                 System.out.println("startx:" + startx + "\t starty:" + starty+ "\tendX:" + endx + "\t Endy:" + endy);
-                i.addAndGet(1);
+                i.addAndGet(1); // Der counter wird erhöht
             });
             timeline = new Timeline(keyFrame);
-            timeline.setCycleCount(nodeArray.size()-1);
+            timeline.setCycleCount(nodeArray.size()-1); // Die Timeline soll so oft wiederholt werden, bis das nodeArray vollends durchgegangen wurde
             timeline.play();
     }
 }
