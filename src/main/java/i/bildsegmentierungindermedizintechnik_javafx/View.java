c577554d894302f01465f9b2f4cd0d3f76dc8f5b
reney <rene.weissteiner@gmail.com>
2023-02-25 18:54:43 +0100

    o/

--- src/main/java/i/bildsegmentierungindermedizintechnik_javafx/View.java
+++ src/main/java/i/bildsegmentierungindermedizintechnik_javafx/View.java
@@ -3,9 +3,6 @@ package i.bildsegmentierungindermedizintechnik_javafx;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.paint.Paint;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class View extends Application {
@@ -14,28 +11,6 @@ public class View extends Application {
     ViewController viewControler;
     final int windowSizeX = 500;  // Einfache Änderung der Größe des Fensters (nur am Start möglich)
     final int windowSizeY = 550;
-
-    // Eine Methode um die Rechtecke einfacher zu erstellen, die für die Dekoration des Fensters benötigt werden
-    public Rectangle createRectangle(int sizex, int sizey, int x, int y, String color){
-        Rectangle rectangle = new Rectangle();
-        rectangle.setWidth(sizex);
-        rectangle.setHeight(sizey);
-        rectangle.setX(x);
-        rectangle.setY(y);
-        rectangle.setFill(Paint.valueOf(color));
-        return rectangle;
-    }
-    //Da im ViewController öfters eine Linie gezeichnet werden muss, wurde dies hier als methode geschrieben,
-    // um den späteren Prozess zu vereinfachen
-    public Line createLine(int startx, int starty, int endx, int endy) {
-        Line line = new Line();
-        line.setStartX(startx);
-        line.setStartY(starty);
-        line.setEndX(endx);
-        line.setEndY(endy);
-        line.setStroke(Paint.valueOf("#ff0000")); // Farbe der Linie
-        return line;
-    }
 
     @Override
     public void start(Stage stage){
--- src/main/java/i/bildsegmentierungindermedizintechnik_javafx/ViewController.java
+++ src/main/java/i/bildsegmentierungindermedizintechnik_javafx/ViewController.java
@@ -6,6 +6,8 @@ import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 //import javafx.scene.image.Image;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
@@ -18,7 +20,7 @@ public class ViewController {
     ArrayList<Node> nodes; // Nodes mit Koordinaten zum kürzesten Weg
     //Image polarCord;
 
-    public ViewController(View view) {
+    public ViewController(View view) { // Im Konstruktor des ViewCOntrollers wird einerseits ein View übergeben, über den man das Fenster bearbeiten kann
         this.view = view;
         //Anzeigen des Polarbildes
 
@@ -38,8 +40,8 @@ public class ViewController {
     }
     public void displayLayout(){
         //Deko Rechtecke
-        recArray.add(view.createRectangle(view.windowSizeX, view.windowSizeY/6, 0, 0, "#c2f2ee"));
-        recArray.add(view.createRectangle(400,400,view.windowSizeX/2-200,view.windowSizeY/6+25,"#c2f2ee"));
+        recArray.add(createRectangle(view.windowSizeX, view.windowSizeY/6, 0, 0, "#c2f2ee"));
+        recArray.add(createRectangle(400,400,view.windowSizeX/2-200,view.windowSizeY/6+25,"#c2f2ee"));
         view.view.getChildren().addAll(recArray); // zeigt das gesamte recArray an
 
         //Button
@@ -67,12 +69,34 @@ public class ViewController {
                 int startY = nodeArray.get(i.get() -1).y + beginOfDisplayY;
                 int endX = nodeArray.get(i.get()).x + beginOfDisplayX;
                 int endY = nodeArray.get(i.get()).y + beginOfDisplayY;
-                view.view.getChildren().add(view.createLine(startX, startY,  endX, endY)); //Zeigt die Linie final auf dem Fenster an
+                view.view.getChildren().add(createLine(startX, startY,  endX, endY)); //Zeigt die Linie final auf dem Fenster an
                 System.out.println("startX:" + startX + "\t startY:" + startY+ "\tendX:" + endX + "\t endY:" + endY);
                 i.addAndGet(1); // Der counter wird erhöht
             });
             timeline = new Timeline(keyFrame);
             timeline.setCycleCount(nodeArray.size()-1); // Die Timeline soll so oft wiederholt werden, bis das nodeArray vollends durchgegangen wurde
             timeline.play();
+    }
+
+    // Eine Methode um die Rechtecke einfacher zu erstellen, die für die Dekoration des Fensters benötigt werden
+    public Rectangle createRectangle(int sizex, int sizey, int x, int y, String color){
+        Rectangle rectangle = new Rectangle();
+        rectangle.setWidth(sizex);
+        rectangle.setHeight(sizey);
+        rectangle.setX(x);
+        rectangle.setY(y);
+        rectangle.setFill(Paint.valueOf(color));
+        return rectangle;
+    }
+    //Da im ViewController öfters eine Linie gezeichnet werden muss, wurde dies hier als methode geschrieben,
+    // um den späteren Prozess zu vereinfachen
+    public Line createLine(int startx, int starty, int endx, int endy) {
+        Line line = new Line();
+        line.setStartX(startx);
+        line.setStartY(starty);
+        line.setEndX(endx);
+        line.setEndY(endy);
+        line.setStroke(Paint.valueOf("#ff0000")); // Farbe der Linie
+        return line;
     }
 }
